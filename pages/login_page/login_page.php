3c93f30334a8695a4600042e341e7befa92dbffc
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-09-03 18:45:49 -0400

    desktop home / log in page added

--- pages/login_page/login_page.php
+++ pages/login_page/login_page.php
@@ -4,19 +4,16 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../components/navbar/navbar_styles.css">
-        <link rel="stylesheet" href="../components/user_auth_desktop/user_auth_desktop.css">
+       
+        <link rel="stylesheet" href="../../components/user_auth_desktop/user_auth_desktop.css">
         <link rel="stylesheet" href="styles.css">
         <title>Document</title>
     </head>
     <body>
-        <div id="nav">
-        <?php include('../components/navbar/navbar.html') ?>
-</div>
         <div id="content">
             <div id="auth">
             <?php 
-        include('../components/user_auth_desktop/user_auth_desktop.html')?>
+        include('../../components/user_auth_desktop/user_auth_desktop.html')?>
 </div>
        
         </div>
