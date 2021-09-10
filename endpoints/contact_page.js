5f97f40b00dcd6dfa729ab4d9b45016703126134
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-09-10 12:21:07 -0400

    adding in API formatting

--- endpoints/contact_page.js
+++ endpoints/contact_page.js
@@ -1,3 +1,49 @@
+// function doAction()
+// {
+	
+// 	// Get the values for the HTML
+
+// 	// Create the JS object to send to the PHP and convert it to JSON
+// 	var obj = 
+// 	{	
+
+// 	};
+
+// 	var input = JSON.stringify();
+	
+// 	// Open up a new Request
+// 	var url = urlBase + '<FileName>' + extension;
+// 	var xhr = new XMLHttpRequest();
+// 	xhr.open("POST", url, true);
+// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+// 	// Send the Request 
+// 	try
+// 	{
+// 		xhr.onreadystatechange = function() 
+// 		{
+// 			// If the request is valid :
+// 			if (this.readyState == 4 && this.status == 200) 
+// 			{
+// 				// Get the response
+// 				var jsonObject = JSON.parse( xhr.responseText);
+				
+				
+
+// 				saveCookie();
+	
+// 			}
+// 		};
+// 		xhr.send(jsonPayload);
+// 	}
+// 	// If the request is invalid:
+// 	catch(err)
+// 	{
+		
+// 	}
+// 	// Any aditional actions that need to happen after the process is complete: 
+	
+// }
 function searchContacts()
 {
 
