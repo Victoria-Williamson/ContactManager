704516c0de4d088e68a602cfc615e4a2bea58471
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-09-10 15:55:16 -0400

    basics for contact page

--- endpoints/contact_page.js
+++ endpoints/contact_page.js
@@ -59,9 +59,48 @@ function deleteContact()
 
 }
 
-function addContact()
+function addContact(firstName,lastName,phoneNumber,userEmail)
 {
+    var card = document.createElement('div');
+    card.id = "contact-card";
 
+    var cardImage = document.createElement('div');
+    cardImage.id = "contact-image";
+
+    var initials = document.createElement('text');
+    initials.id = "initials";
+    initials.innerHTML = firstName.charAt(0) + lastName.charAt(0);
+
+    cardImage.appendChild(initials);
+    card.appendChild(cardImage);
+
+    var user_information = document.createElement('div');
+    user_information.id = "user-information";
+
+    var name = document.createElement('text');
+    name.id = "user-name";
+    name.innerHTML = firstName + " " + lastName;
+
+    var phone = document.createElement('a');
+    phone.className="info";
+    phone.href="tel:+" + phoneNumber;
+    phone.innerHTML = "Phone: " + "(" + phoneNumber.slice(0,3) + ")"  + phoneNumber.slice(3,6) + "-" + phoneNumber.slice(6);
+
+    var email = document.createElement('a');
+    email.className="info";
+    email.href="mailto:" + userEmail;
+    email.innerHTML = "Email: " + userEmail;
+
+    user_information.appendChild(name);
+    user_information.appendChild(phone);
+    user_information.append(email);
+
+    card.appendChild(user_information);
+
+
+
+    var list = document.getElementById("contact-list");
+    list.append(card);
 }
 
 function pageLoad()
