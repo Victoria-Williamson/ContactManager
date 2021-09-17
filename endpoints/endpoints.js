5c231b8a97c04667369675b6d2653bdb7a7cdde9
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-09-17 16:22:44 -0400

    begin adding in edit contact response

--- endpoints/endpoints.js
+++ endpoints/endpoints.js
@@ -273,15 +273,31 @@ function searchContacts()
 
 }
 
-function editContact()
+function showEditContact(event)
 {
+    var image = event.firstChild;
+    var info = event.lastChildl
+   
+    console.log(typeof image);
+
+    if (event.id !== "contact-card")
+        return;
+    console.log(event.getElementById("contact-image"));
     var parent = document.createElement('div');
     parent.id = "add-contact-div";
     parent.className = "modify-div";
 
     var inner = document.createElement("div");
     inner.className = 'center-div';
-    inner.innerHTML = '<div id="card"> <div id="contact-image-big"><text id="initials"> ? </text> </div><div class="form"><label for="name">First Name</label><input id="name" type="text" value="" placeholder="John" name="name"></div><div class="form"><label for="name">Lirst Name</label> <input id="name" type="text" value="" placeholder="Doe" name="name"></div><div class="form"><label for="name">Phone Number</label><input id="name" type="text" value="" placeholder="555-555-5555" name="number"></div><div class="form"><label for="name">Email</label><input id="name" type="text" value="" placeholder="email@domain.com" name="email"></div><div id="modify-buttons"><button id="save"> Save </button><button id="delete"> Delete </button></div><div id="exit"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 2.01429L17.9857 0L10 7.98571L2.01429 0L0 2.01429L7.98571 10L0 17.9857L2.01429 20L10 12.0143L17.9857 20L20 17.9857L12.0143 10L20 2.01429Z" fill="#919191"/></svg></div></div>';
+    inner.innerHTML = '<div id="card"><div id="contact-image-big"><text id="initials"> ? </text></div><div class="form"><label for="first-add">First Name</label><input id="first-add" type="text" value="" placeholder="John" name="name"></div><div class="form"><label for="last-add">Last Name</label><input id="last-add" type="text" value="" placeholder="Doe" name="name"></div><div class="form"><label for="number-add">Phone Number</label><input id="number-add type="text" value="" placeholder="555-555-5555" name="number"></div><div class="form"><label for="email-add">Email</label><input id="email-add" type="text" value="" placeholder="email@domain.com" name="email"></div><div id="modify-buttons"><button id="save" onclick="editContact()"> Save </button><button id="delete" onclick="deleteContact()"> Delete </button></div><button id="exit" onclick="hideEditContact()"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 2.01429L17.9857 0L10 7.98571L2.01429 0L0 2.01429L7.98571 10L0 17.9857L2.01429 20L10 12.0143L17.9857 20L20 17.9857L12.0143 10L20 2.01429Z" fill="#919191"/></svg></div></div>';
+
+    parent.appendChild(inner);
+
+    var initials = event.getElementById("initials").innerHTML;
+    inner.getElementById("initials").innerHTML = initials;
+    var loc = document.getElementById("action-div");
+    loc.appendChild(parent);
+    loc.style.display = "block";
 }
 
 function deleteContact()
@@ -307,6 +323,12 @@ function showAddContact()
     loc.style.display = "block";
 }
 
+function hideEditContact()
+{
+    var action = document.getElementById("action-div");
+    action.innerHTML = "";
+    action.style.display = "none";
+}
 function hideAddContact()
 {
     var action = document.getElementById("action-div");
@@ -316,6 +338,8 @@ function hideAddContact()
 function addContact()
 {
     
+    console.log(document.getElementById("add-contact-div"));
+
     hideAddContact();
     var firstName = "vic";
     var lastName = "last";
@@ -326,7 +350,11 @@ function addContact()
     console.log(info);
     var card = document.createElement('div');
     card.id = "contact-card";
-
+    card.onclick = function (e)
+    {
+        console.log(e.target);
+        showEditContact(e.target);
+    }
     var cardImage = document.createElement('div');
     cardImage.id = "contact-image";
 
@@ -358,13 +386,14 @@ function addContact()
     user_information.appendChild(phone);
     user_information.append(email);
 
-     card.appendChild(user_information);
-     console.log(document.getElementById('action-div'));
-     console.log(document.getElementById('contact-div'));
-        var parent = document.getElementById('contact-div');
-        console.log(parent);
-        var list = document.getElementById("contact-list");
-        list.append(card);
+    card.appendChild(user_information);
+    console.log(document.getElementById('action-div'));
+    console.log(document.getElementById('contact-div'));
+    var parent = document.getElementById('contact-div');
+    console.log(parent);
+    console.log(document.getElementsByClassName("contact-list"))
+    var list = document.getElementById("contact-list");
+    list.append(card);
     
 }
 
