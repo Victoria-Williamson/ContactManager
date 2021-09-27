e12f569e9bb6a04df73f6132298bc2cef149ab39
Victoria Williamson <vicwill01@knights.ucf.edu>
2021-09-27 12:05:57 -0400

    reverting back API

--- API/search.php
+++ API/search.php
@@ -1,18 +1,15 @@
 <?php
 
-// Made by ellie :)
+// Make by ellie :) 
 include 'util.php';
 
-    // Take the JSON response and convert it to PHP variable.
     $inData = getRequestInfo();
 
     $resCount = 0;
     $results = array();
 
-    // Establish connection with mysqli(host, username, password, database).
     $connection = db_connect();
 
-    // Check connection response.
     if ($connection->connect_error) 
     {
         returnWithContactError($connection->connect_error);
@@ -22,20 +19,15 @@ include 'util.php';
         // Search through first and last names that belong to the user. Parenthesis for unambiguity!
         $stmt = $connection->prepare("SELECT * FROM Contacts WHERE (firstName LIKE ? OR lastName LIKE ? )AND uid=?");
         $search = "%" . $inData["search"] . "%";
+        $stmt->bind_param('ssi', $search, $search, $inData['uid']);
 
-        // Bind each (?) variable with it's data.
-        $stmt->bind_param('sisi', $search, $inData['uid'], $search, $inData['uid']);
-
-        // Execute statement.
         $stmt->execute();
-
-        // Data fetched from database to PHP.
+         // Data fetched from database to PHP.
 		$result = $stmt->get_result();
         
         if ($result->num_rows > 0)
         {
-            // Keep searching until there's no more rows.
-            while ($row = $result->fetch_assoc())
+            while($row = $result->fetch_assoc())
             {
                 $results[$resCount] = createObjectContact($row["uid"], $row["cid"],$row["firstName"], $row["lastName"], $row["PhoneNumber"], $row["Email"], "");
 
@@ -46,12 +38,12 @@ include 'util.php';
         else
         {
             returnWithContactError("No contacts matching search");
+            
         }
     }
      // Send results.
      sendResultInfoAsJSON(json_encode($results));
 
-    // Close established connections.
-    $stmt->close();
-    $connection->close();
-?>
+     $stmt->close();
+     $connection->close();
+    ?>
